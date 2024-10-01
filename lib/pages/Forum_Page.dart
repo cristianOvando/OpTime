85404730e9c809fe67ad268096d2222530e53673
cristianOvando <ovandogomez60@gmail.com>
2024-10-01 15:59:42 -0600

    Interface navigation

---
+++ lib/pages/Forum_Page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ForumPage extends StatelessWidget {
+  const ForumPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Contenido del foro'),
+    );
+  }
+}
